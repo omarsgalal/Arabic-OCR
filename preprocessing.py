f2fac344d27dc91cbd42709b24c4ec9cc174ce9f
omarsgalal <omarsgalal4@gmail.com>
2019-12-02 16:15:00 +0200

    8000 from 20000

--- preprocessing.py
+++ preprocessing.py
@@ -23,7 +23,7 @@ def correctSkew(image):
 
 def getLines(image):
     hist = cv2.reduce(image,1, cv2.REDUCE_AVG).reshape(-1)
-    avgFilter = np.array([0.2, 0.2, 0.2])
+    avgFilter = np.array([0.25, 0.25, 0.25, 0.25])
     hist = np.convolve(hist, avgFilter, 'same')
     
     th = 0
@@ -45,9 +45,9 @@ def getLines(image):
 
 def getWords(lineImage):
     hist = cv2.reduce(lineImage, 0, cv2.REDUCE_AVG).reshape(-1)
-    avgFilter = np.array([0.2, 0.2, 0.2])
+    avgFilter = np.array([1/3, 1/3, 1/3])
     hist = np.convolve(hist, avgFilter, 'same')
-    th = 0
+    th = 1
     H,W = lineImage.shape[:2]
     uppers = [y for y in range(W-1) if hist[y]<=th and hist[y+1]>th]
     lowers = [y for y in range(W-1) if hist[y]>th and hist[y+1]<=th]
@@ -55,8 +55,19 @@ def getWords(lineImage):
     # colored = cv2.cvtColor(lineImage, cv2.COLOR_GRAY2BGR)
 
     if len(uppers) == len(lowers):
-        for i in range(len(uppers) - 1, -1, -1):
-            wordImages.append(lineImage[:, uppers[i]:lowers[i]])
+        i = len(uppers) - 1
+        j = len(lowers) - 1
+        while i >= 0:
+            smallWord = lineImage[:, uppers[i]:lowers[i]]
+            if smallWord.shape[1] > 8 or i == 0:
+                # if i == 0 and i == j and len(wordImages) > 0:
+                #     wordImages[-1] = lineImage[:, uppers[i]:lowers[j + 1]]
+                # else:
+                wordImages.append(lineImage[:, uppers[i]:lowers[j]])
+                i -= 1
+                j = i
+            else:
+                i -= 1
             # cv2.line(colored, (uppers[i], 0), (uppers[i], H), (255,0,0), 1)
             # cv2.line(colored, (lowers[i], 0), (lowers[i], H), (0,255,0), 1)
     else:
@@ -69,10 +80,12 @@ def preprocess(image):
     rotatedImage = correctSkew(binarizedImage)
     _, _, linesImages = getLines(rotatedImage)
     linesOfWords = []
+    numWords = 0
     for img in linesImages:
         _, _, wordImages = getWords(img)
         linesOfWords.append(wordImages)
-    return linesOfWords
+        numWords += len(wordImages)
+    return linesOfWords, numWords, linesImages
 
 if __name__ == "__main__":
     # import os
@@ -81,9 +94,10 @@ if __name__ == "__main__":
     #     image = cv2.imread(f"images/{i}")
     #     preprocess(image)
 
-    image = cv2.imread("images/capr2.png")
+    image = cv2.imread("cjan1801.png")
     linesOfWords = preprocess(image)
-    for line in linesOfWords:
-        for word in line:
-            cv2.imshow("word", word)
-            cv2.waitKey(0)
+    for j, line in enumerate(linesOfWords):
+        for i, word in enumerate(line):
+            # cv2.imshow("word", word)
+            # cv2.waitKey(0)
+            cv2.imwrite(f"output/{j}_{i}.png", word)
