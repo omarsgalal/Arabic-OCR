cab25544bc90a1ddc1b9fc71178123ac50f3dade
omarsgalal <omarsgalal4@gmail.com>
2019-12-07 17:59:32 +0200

    modifying baseline in preprocessing

--- checkwordsegmentations.py
+++ checkwordsegmentations.py
@@ -6,7 +6,7 @@ import progressbar
 
 pathText = "Dataset/text/"
 pathImg = "Dataset/scanned/"
-fileName = "caug1008.png"
+fileName = "capr114.png"
 
 def getText(fileName):
     f = codecs.open(fileName, encoding='utf-8')
@@ -40,51 +40,52 @@ def checkImgwithText(fileName, pathText, pathImg):
     image = cv2.imread(pathImg + fileName)
     linesOfWords, numWords, linesImages = preprocess(image)
     wordsText = getText(pathText + fileName[:-4] + ".txt")
-    # if numWords != len(wordsText):
-    #     if not os.path.exists(fileName[:-4]):
-    #         os.mkdir(fileName[:-4])
-    #     if not os.path.exists(f"{fileName[:-4]}/smalls"):
-    #         os.mkdir(f"{fileName[:-4]}/smalls")
-    #     if not os.path.exists(f"{fileName[:-4]}/smalls/lines"):
-    #         os.mkdir(f"{fileName[:-4]}/smalls/lines")
-    #     for j, line in enumerate(linesOfWords):
-    #         for i, word in enumerate(line):
-    #             cv2.imwrite(f"{fileName[:-4]}/{j}_{i}.png", word)
-    #             if word.shape[1] < 10:
-    #                 cv2.imwrite(f"{fileName[:-4]}/smalls/{j}_{i}.png", word)
-    #                 cv2.imwrite(f"{fileName[:-4]}/smalls/lines/{j}.png", linesImages[j])
-    #     file = open(f"{fileName[:-4]}/words.txt", 'w')
-    #     file2 = open(f"{fileName[:-4]}/smalls.txt", 'w')
-    #     for i in wordsText:
-    #         file.write(f"{i}\n")
-    #         if len(i) <= 1:
-    #             file2.write(f"{i}\n")
-    #     file.close()
-    #     file2.close()
+
+    if numWords != len(wordsText):
+        if not os.path.exists(fileName[:-4]):
+            os.mkdir(fileName[:-4])
+        if not os.path.exists(f"{fileName[:-4]}/smalls"):
+            os.mkdir(f"{fileName[:-4]}/smalls")
+        if not os.path.exists(f"{fileName[:-4]}/smalls/lines"):
+            os.mkdir(f"{fileName[:-4]}/smalls/lines")
+        for j, line in enumerate(linesOfWords):
+            for i, word in enumerate(line):
+                cv2.imwrite(f"{fileName[:-4]}/{j}_{i}.png", word)
+                if word.shape[1] < 10:
+                    cv2.imwrite(f"{fileName[:-4]}/smalls/{j}_{i}.png", word)
+                    cv2.imwrite(f"{fileName[:-4]}/smalls/lines/{j}.png", linesImages[j])
+        file = open(f"{fileName[:-4]}/words.txt", 'w')
+        file2 = open(f"{fileName[:-4]}/smalls.txt", 'w')
+        for i in wordsText:
+            file.write(f"{i}\n")
+            if len(i) <= 1:
+                file2.write(f"{i}\n")
+        file.close()
+        file2.close()
     # return numWords == len(wordsText)
     return numWords, len(wordsText)
 
-# print(checkImgwithText(fileName, pathText, pathImg))
+print(checkImgwithText(fileName, pathText, pathImg))
 
-import os
-imagesNames = os.listdir(pathImg)[4000:]
-errorsDict = {}
-maxError = 0
-minError = 0
-numCorrect = 0
-with progressbar.ProgressBar(max_value=len(imagesNames)) as bar:
-    for i, imgName in enumerate(imagesNames):
-        wordsImg, wordsTxt = checkImgwithText(imgName, pathText, pathImg)
-        errorsDict[imgName] = wordsImg - wordsTxt
-        numCorrect += int(errorsDict[imgName] == 0)
-        maxError = max(maxError, errorsDict[imgName])
-        minError = min(minError, errorsDict[imgName])
-        bar.update(i)
+# import os
+# imagesNames = os.listdir(pathImg)[:]
+# errorsDict = {}
+# maxError = 0
+# minError = 0
+# numCorrect = 0
+# with progressbar.ProgressBar(max_value=len(imagesNames)) as bar:
+#     for i, imgName in enumerate(imagesNames):
+#         wordsImg, wordsTxt = checkImgwithText(imgName, pathText, pathImg)
+#         errorsDict[imgName] = wordsImg - wordsTxt
+#         numCorrect += int(errorsDict[imgName] == 0)
+#         maxError = max(maxError, errorsDict[imgName])
+#         minError = min(minError, errorsDict[imgName])
+#         bar.update(i)
 
-f = open("errorsdict_4000_all.txt", 'w')
-f.write(f"total correct: {numCorrect}\n")
-f.write(f"max Error: {maxError}\n")
-f.write(f"min Error: {minError}\n")
-for key, value in errorsDict.items():
-    f.write(f"{key}    {value}\n")
-f.close()
+# f = open("errorsdict_all.txt", 'w')
+# f.write(f"total correct: {numCorrect}\n")
+# f.write(f"max Error: {maxError}\n")
+# f.write(f"min Error: {minError}\n")
+# for key, value in errorsDict.items():
+#     f.write(f"{key}    {value}\n")
+# f.close()
--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,18 @@
 import numpy as np
 import cv2
+from scipy.ndimage import interpolation as inter
+import progressbar
 
 def binarizeImage(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.bitwise_not(gray)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     return thresh
+
+def getBaseline(image):
+    hist = np.sum(image, axis=1)
+    # nonzeros = np.nonzero(hist)
+    return np.argmax(hist)#, nonzeros[0][0], nonzeros[0][-1]
 
 def correctSkew(image):
     coords = np.column_stack(np.where(image > 0))
@@ -23,6 +30,28 @@ def correctSkew(image):
     # print (rotated)
     return rotated
 
+def find_score(arr, angle):
+    data = inter.rotate(arr, angle, reshape=False, order=0)
+    hist = np.sum(data, axis=1)
+    score = np.sum((hist[1:] - hist[:-1]) ** 2)
+    return hist, score
+
+def correctSkew2(image):
+    delta = 1
+    limit = 5
+    angles = np.arange(-limit, limit+delta, delta)
+    scores = []
+    for angle in angles:
+        hist, score = find_score(image, angle)
+        scores.append(score)
+
+    best_score = max(scores)
+    best_angle = angles[scores.index(best_score)]
+
+    # correct skew
+    data = inter.rotate(image, best_angle, reshape=False, order=0)
+    return data
+
 def getLines(image):
     hist = cv2.reduce(image,1, cv2.REDUCE_AVG).reshape(-1)
     avgFilter = np.array([0.25, 0.25, 0.25, 0.25])
@@ -34,16 +63,46 @@ def getLines(image):
     lowers = [y for y in range(H-1) if hist[y]>th and hist[y+1]<=th]
     # rotated = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     linesImages = []
+    maxAbovebaseline = 0
+    maxBelowbaseline = 0
+    avgAbovebaseline = 0
+    avgBelowbaseline = 0
 
     if len(uppers) == len(lowers):
         for i in range(len(uppers)):
-            linesImages.append(image[uppers[i]:lowers[i], :])
+            lineImageI = image[uppers[i]:lowers[i], :]
+            baseline = getBaseline(lineImageI)
+            lineImageI = lineImageI[max(baseline - 13, 0): min(baseline + 9, lineImageI.shape[0] + 1), :]
+            if lineImageI.shape[0] < 8 + 13 + 1: 
+                newLine = np.zeros(( 8 + 13 + 1, lineImageI.shape[1]))
+                #to be modified
+                newLine[1:lineImageI.shape[0] + 1, :] = lineImageI
+                lineImageI = newLine
+            assert lineImageI.shape[0] == 8 + 13 + 1
+            linesImages.append(lineImageI)
+            
+            # baseline += uppers[i]
+            # maxAbovebaseline = max(maxAbovebaseline, baseline - uppers[i])
+            # maxBelowbaseline = max(maxBelowbaseline, lowers[i] - baseline)
+            # avgAbovebaseline += baseline - uppers[i]
+            # avgBelowbaseline += lowers[i] - baseline
+            
+            # maxAbovebaseline = max(maxAbovebaseline, baseline - up)
+            # maxBelowbaseline = max(maxBelowbaseline, low - baseline)
+            # avgAbovebaseline += baseline - up
+            # avgBelowbaseline += low - baseline
             # cv2.line(rotated, (0,uppers[i]), (W, uppers[i]), (255,0,0), 1)
             # cv2.line(rotated, (0,lowers[i]), (W, lowers[i]), (0,255,0), 1)
     else:
         raise Exception("Uppers not equal Lowers in lines")
 
-    return uppers, lowers, linesImages
+    stats = [0, 0, 0, 0]
+    if len(uppers) != 0:
+        avgAbovebaseline /= len(uppers)
+        avgBelowbaseline /= len(uppers)
+        stats = [maxAbovebaseline, maxBelowbaseline, avgAbovebaseline, avgBelowbaseline]
+
+    return uppers, lowers, linesImages, stats
 
 def getWords(lineImage):
     hist = cv2.reduce(lineImage, 0, cv2.REDUCE_AVG).reshape(-1)
@@ -80,7 +139,7 @@ def getWords(lineImage):
 def preprocess(image):
     binarizedImage = binarizeImage(image)
     rotatedImage = correctSkew(binarizedImage)
-    _, _, linesImages = getLines(rotatedImage)
+    _, _, linesImages, _ = getLines(rotatedImage)
     linesOfWords = []
     numWords = 0
     for img in linesImages:
@@ -89,6 +148,29 @@ def preprocess(image):
         numWords += len(wordImages)
     return linesOfWords, numWords, linesImages
 
+def getAvgMaxMinBaseline():
+    import os
+    x = os.listdir("Dataset/scanned/")[0:1000]
+    f = open("stats.txt", 'w')
+    f.write("\t\t\tmaxAbove\tmaxBelow\tavgAbove\tavgBelow\n")
+    allstats = [0, 0, 0, 0]
+    with progressbar.ProgressBar(max_value=len(x)) as bar:
+        for j, i in enumerate(x):
+            image = cv2.imread(f"Dataset/scanned/{i}")
+            binarizedImage = binarizeImage(image)
+            rotatedImage = correctSkew(binarizedImage)
+            _, _, _, stats = getLines(rotatedImage)
+            f.write(f"{i}\t{stats[0]}\t{stats[1]}\t{stats[2]}\t{stats[3]}\n")
+            allstats[0] = max(allstats[0], stats[0])
+            allstats[1] = max(allstats[1], stats[1])
+            allstats[2] += stats[2]
+            allstats[3] += stats[3]
+            bar.update(j)
+    allstats[2] /= len(x)
+    allstats[3] /= len(x)
+    f.write(f"\t\t\t{allstats[0]}\t{allstats[1]}\t{allstats[2]}\t{allstats[3]}")
+    f.close()
+
 if __name__ == "__main__":
     # import os
     # x = os.listdir("images")
@@ -96,12 +178,14 @@ if __name__ == "__main__":
     #     image = cv2.imread(f"images/{i}")
     #     preprocess(image)
 
-    import sys
-    np.set_printoptions(threshold=sys.maxsize)
-    image = cv2.imread("Dataset/scanned/capr2.png")
-    linesOfWords, numWords, linesImages = preprocess(image)
-    for j, line in enumerate(linesOfWords):
-        for i, word in enumerate(line):
-            # cv2.imshow("word", word)
-            # cv2.waitKey(0)
-            cv2.imwrite(f"output/{j}_{i}.png", word)
+    # import sys
+    # np.set_printoptions(threshold=sys.maxsize)
+    # image = cv2.imread("Dataset/scanned/capr100.png")
+    # linesOfWords, numWords, linesImages = preprocess(image)
+    # for j, line in enumerate(linesOfWords):
+    #     for i, word in enumerate(line):
+    #         # cv2.imshow("word", word)
+    #         # cv2.waitKey(0)
+    #         cv2.imwrite(f"output/{j}_{i}.png", word)
+
+    getAvgMaxMinBaseline()
